cdae5f3437ed5db4dde758f82613ec47079314f5
Kamgue fokoua blondin <fokouak@gmail.com>
2020-09-27 17:37:55 +0200

    tame

--- index.php
+++ index.php
@@ -1,15 +1,210 @@
 <?php
-
-/**
- * File to handle all API requests
- * Accepts GET and POST
- * 
- * Each request will be identified by TAG
- * Response will be JSON data
-
-  /**
- * check for POST request 
- */
-
-echo "Blondin";
+//blondin
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Timeless One-Page HTML Template</title>
+<!-- 
+
+Timeless Template 
+
+http://www.templatemo.com/tm-517-timeless
+
+-->
+    <!-- load CSS -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">
+    <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- https://getbootstrap.com/ -->
+    <link rel="stylesheet" href="css/templatemo-style.css">
+    <!-- Templatemo style -->
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <header class="text-center tm-site-header">
+                    <div class="tm-site-logo"><img src="img/logo.png" alt="Image" class="rounded-circle tm-img-timeline"></div>
+                    <h1 class="pl-4 tm-site-title">SATECH</h1>
+                </header>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="tm-video-container">
+                    <video id="tm-welcome-video" class="tm-welcome-video" autoplay="" loop="" muted="">
+                        <source src="videos/video.mp4" type="video/mp4"> Votre navigateur ne supporte pas.
+                        </video>
+                        <div id="tm-video-loader"></div>
+                        <div id="tm-video-text-overlay" class="tm-video-text-overlay d-none">
+                            <h1>
+                                <div id="rotate" class="tm-video-text">
+                                    <div>Application Android</div>
+                                    <div>Site web</div>
+                                    <div>Rapport de stage</div>
+                                    <div>SATECH télécharger</div>
+                                </div>
+                            </h1>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container tm-container-2">
+            <div class="row">
+                <div class="col-lg-12">
+                    <a href="db/satech.apk" download ><h2 class="tm-welcome-text">Accueil en Entreprise & Application SATECH cliquez pour télécharger</h2></a>
+                </div>
+            </div>
+            <div class="row tm-section-mb">
+                <div class="col-lg-12">
+                    <div class=" tm-timeline-item">
+                        <div class="tm-timeline-item-inner">
+                            <img src="img/img-01.jpg" alt="Image" class="rounded-circle tm-img-timeline">
+                            <div class="tm-timeline-connector">
+                                <p class="mb-0">&nbsp;</p>
+                            </div>
+                            <div class="tm-timeline-description-wrap">
+                                <div class="tm-bg-dark tm-timeline-description">
+                                    <h3 class="tm-text-green tm-font-400">Nulla venenatis purus nec quam</h3>
+                                    <p>Pour notre stage, l’ASECNA  a bien voulu nous compte parmi les leur. C’est ainsi que dès notre arrivée à l’aérodrome de l’ASECNA-Garoua, le Lundi 03 Août 2020 à 8H00, plus précisément dans un bureau appelé Atelier Radio dans lequel ASECNA accueillent ses stagiaires.</p>
+                                    <p class="tm-text-green float-right mb-0">ASECNA accueillent ses stagiaires</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tm-timeline-connector-vertical"></div>
+                    </div>
+
+                    <div class="tm-timeline-item">
+                        <div class="tm-timeline-item-inner">
+                            <img src="img/img-02.jpg" alt="Image" class="rounded-circle tm-img-timeline">
+                            <div class="tm-timeline-connector">
+                                <p class="mb-0">&nbsp;</p>
+                            </div>
+                            <div class="tm-timeline-description-wrap">
+                                <div class="tm-bg-dark-light tm-timeline-description">
+                                    <h3 class="tm-text-cyan tm-font-400">Présentation de l’ASECNA</h3>
+                                    <p> L’Agence pour la Sécurité de la Navigation Aérienne en Afrique et à Madagascar (ASECNA) a vu le jour le 12 Décembre 1959 à Saint-Louis au Sénégal. Compte  tenu de l’internationalisation du contrôle de la circulation aérienne, les Etats signataire de la convention  de SAINT-LOUIS modifient cette dernière en adoptant celle de Dakar le 25 Octobre 1974.</p>
+                                    <p class="tm-text-cyan float-right mb-0">Historique</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tm-timeline-connector-vertical"></div>
+                    </div>
+
+                    <div class="tm-timeline-item">
+                        <div class="tm-timeline-item-inner">
+                            <img src="img/img-03.jpg" alt="Image" class="rounded-circle tm-img-timeline">
+                            <div class="tm-timeline-connector">
+                                <p class="mb-0">&nbsp;</p>
+                            </div>
+                            <div class="tm-timeline-description-wrap">
+                                <div class="tm-bg-dark tm-timeline-description">
+                                    <h3 class="tm-text-yellow tm-font-400">Situation géographique de l’aérodrome de Garoua</h3>
+                                    <p>L’aéroport international de Garoua est situé dans la région du Nord au Cameroun. Cet aéroport est localisé plus précisément dans le Département de la Bénoué, Arrondissement de Garoua 1er. Il est repéré géographiquement par son point de référence dont les coordonnées sont : (09°20’12’’N ; 013°22’52’’E).</p>
+                                    <p class="tm-text-yellow float-right mb-0">ASECNA) a vu le jour le 12 Décembre 1959</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tm-timeline-connector-vertical"></div>
+                    </div>
+
+                    <div class="tm-timeline-item">
+                        <div class="tm-timeline-item-inner">
+                            <img src="img/img-04.jpg" alt="Image" class="rounded-circle tm-img-timeline">
+                            <div class="tm-timeline-connector">
+                                <p class="mb-0">&nbsp;</p>
+                            </div>
+                            <div class="tm-timeline-description-wrap">
+                                <div class="tm-bg-dark-light tm-timeline-description">
+                                    <h3 class="tm-text-orange tm-font-400">Fonctionnement et organisation de l’aérodrome de Garoua</h3>
+                                    <p>L’aéroport international de Garoua dessert un trafic du type local, national et régional ; trafic qui n’a pas cessé d’évoluer depuis sa création jusqu'à nos jours. Il fonctionne de 7H00 à 20H00 UTC («Universel Time Control »).  L’aérodrome de Garoua dispose des ressources humaines  en fonction des différentes unités.</p>
+                                    <p class="tm-text-orange float-right mb-0">Tableau 2 : Effectif et prise de services</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <!--  row -->
+            <hr>
+            <div class="row tm-section-mb tm-section-mt">
+                <div class="col-lg-4 col-md-4 col-sm-12 pr-lg-5 mb-md-0 mb-4">
+                    <h3 class="mt-2 mb-3 tm-text-gray">Choix du langage de programmation: Java</h3>
+                    <p>Java est un langage de programmation orienté objet, développé par Sun Microsystems et destiné à fonctionner dans une machine virtuelle, il permet de créer des logiciels compatibles avec des nombreux systèmes d’exploitation..</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 pr-lg-5 mb-md-0 mb-4">
+                    <h3 class="mt-2 mb-3 tm-text-gray">Choix de l’architecture de l’application: Client/ Serveur</h3>
+                    <p>Dans l’architecture à trois niveaux, les applications au niveau serveur sont délocalisées, c’est à dire que chaque serveur est spécialisé dans une tâche (serveur web/ serveur de base de données par exemple).</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <h3 class="mt-2 mb-3 tm-text-gray">Besoins non fonctionnel</h3>
+                    <p> Pour lui donner une apparence plus ou moins semblable à celle de l’aérodrome de Garoua, nous utiliserons le logo de l’ASECNA. Nous utiliserons une galerie de couleur dominée par le vert-citron. Pour faciliter l’usage de l’application, nous ferons de formulaires simples et compréhensibles par tous. Au niveau de ce formulaire, nous aurons beaucoup de champs de saisis..</p>
+                </div>
+            </div>
+            <hr>
+            <div class="row tm-section-mt">
+                <div class="col-lg-6">
+                    <h3 class="mb-4 tm-text-gray">Notre application</h3>
+                    <!-- GOOGLE MAP -->
+                    <div id="google-map" class="text-center mb-5">
+                    <!-- How to change your own map point
+                           1. Go to Google Maps
+                           2. Click on your location point
+                           3. Click "Share" and choose "Embed map" tab
+                           4. Copy only URL and paste it within the src="" field below
+                       -->
+                       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3647.3030413476204!2d100.5641230193719!3d13.757206847615207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf51ce6427b7918fc!2sG+Tower!5e0!3m2!1sen!2sth!4v1510722015945"
+                       class="google-map-iframe" width="500" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                   </div>
+               </div>
+               <div class="col-lg-6 mb-5">
+                <h3 class="mb-4 tm-text-gray">Contactez-nous</h3>
+                <form action="#contact" method="post" class="tm-contact-form">
+                    <div class="row">
+                        <div class="form-group col-xl-6">
+                            <input type="text" id="contact_name" name="contact_name" class="form-control" placeholder="Nom..." required/>
+                        </div>
+                        <div class="form-group col-xl-6">
+                            <input type="email" id="contact_email" name="contact_email" class="form-control" placeholder="Email..." required/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <textarea id="contact_message" name="contact_message" class="form-control" rows="6" placeholder="Votre Message..." required></textarea>
+                    </div>
+                    <button type="submit" class="btn  tm-btn-send">Envoyer/button>
+                </form>
+            </div>
+        </div>
+        <hr>
+        <!-- Footer -->
+        <footer class="row mt-5 mb-5">
+            <div class="col-lg-12">
+                <p class="text-center tm-text-gray tm-copyright-text mb-0">Copyright &copy;
+                    <span class="tm-current-year">2019-2020</span> Rédigé et soutenu par DJIJDJOU DJOKO Joséphine Larissa
+                    
+                    | <a href="#" class="tm-text-white">L’IAI- Cameroun</a>
+                </p>
+            </div>
+        </footer>
+    </div>
+    <!-- .container -->
+
+    <script src="js/jquery.min.js"></script>
+    <!-- <script src="js/jquery-3.2.1.slim.min.js"></script> -->
+    <script src="js/templatemo-script.js"></script>
+
+</body>
+</html>
