792eb20a48753b1b8dc278c35b735653fc13d1c0
Kamgue fokoua blondin <fokouak@gmail.com>
2020-09-27 15:20:07 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * File to handle all API requests
+ * Accepts GET and POST
+ * 
+ * Each request will be identified by TAG
+ * Response will be JSON data
+
+  /**
+ * check for POST request 
+ */
+
+echo "Blondin";
+?>
